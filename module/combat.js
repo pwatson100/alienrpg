5862dfb79ed66d309ae787ec3905ac820826ea75
pax <pwatson100@gmail.com>
2021-05-13 13:22:37 +0100

    Combat tracker working as per 0.7.9

--- module/combat.js
+++ module/combat.js
@@ -21,78 +21,76 @@ export default class AlienRPGCombat extends Combat {
     ids = typeof ids === 'string' ? [ids] : ids;
     const currentId = this.combatant.data._id;
     const draw = { 1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false, 8: false, 9: false, 10: false };
-
     // Iterate over Combatants, performing an initiative roll for each
-    const [updates, messages] = ids.reduce(
-      (results, id, i) => {
-        if (i > 9) {
-          ui.notifications.error(
-            `You can only automatically roll Initiative for 10 PCs/NPCs.  Roll initiative manually for the remaining actors or split in to multiple encounters in the Combat Tracker`
-          );
-          return results;
+    // const [updates, messages] = ids.reduce(
+
+    let drawn = 0;
+    const updates = [];
+    const messages = [];
+    for (let [i, id] of ids.entries()) {
+      // Get Combatant data
+      if (drawn < 10) {
+        const combatant = this.combatants.get(id);
+        if (!combatant?.isOwner) return results;
+
+        const cf = formula || combatant._getInitiativeFormula();
+        let broll = this.getInit(combatant, cf, updates);
+        while (draw[broll.total]) {
+          broll = this.getInit(combatant, cf, updates);
         }
 
-        let [updates, messages] = results;
+        draw[broll.total] = true;
+        drawn++;
+        // updates.push({ _id: id, initiative: roll.total });
+        // let broll = this.getInit(combatant, cf, updates);
+        updates.push({ _id: id, initiative: broll.total });
 
-        // Get Combatant data
-        const c = this.combatants.get(id);
-        if (!c || !c.isOwner) return results;
-
-        // Roll initiative
-        const cf = formula || c._getInitiativeFormula();
-
-        let roll = this.getInit(c, cf, updates);
-        while (draw[roll.total]) {
-          roll = this.getInit(c, cf, updates);
-        }
-        draw[roll.total] = true;
-        updates.push({ _id: id, initiative: roll.total });
-
-        // Determine the roll mode
-        let rollMode = messageOptions.rollMode || game.settings.get('core', 'rollMode');
-        if ((c.token.hidden || c.hidden) && rollMode === 'roll') rollMode = 'gmroll';
-        {
-          /* <img width="125" height="175" src="systems/alienrpg/images/cards/card-${roll.total}.png" style="horizontal-align:centre"> */
-        }
-        let cardPath = `<div style="text-align: center;"><img width="125" height="175" src="systems/alienrpg/images/cards/card-${roll.total}.png"></div>`;
-        // if (!game.settings.get(ssmoduleKey, 'imported' || !game.settings.get(crmoduleKey, 'imported')){
-        try {
-          if (game.settings.get(crmoduleKey, 'imported')) {
-            cardPath = `<div style="text-align: center;"><img width="125" height="175" src="modules/alienrpg-corerules/images/cards/card-${roll.total}.png"></div>`;
+        if (!game.settings.get('alienrpg', 'alienrpgHideInitChat')) {
+          // Determine the roll mode
+          let rollMode = messageOptions.rollMode || game.settings.get('core', 'rollMode');
+          if ((combatant.token.hidden || combatant.hidden) && rollMode === 'roll') {
+            rollMode = 'gmroll';
           }
-        } catch (error) {
+          let cardPath = `<div style="text-align: center;"><img width="125" height="175" src="systems/alienrpg/images/cards/card-${broll.total}.png"></div>`;
           try {
-            if (game.settings.get(ssmoduleKey, 'imported')) {
-              cardPath = `<div style="text-align: center;"><img width="125" height="175" src="modules/alienrpg-starterset/images/cards/card-${roll.total}.png"></div>`;
+            if (game.settings.get(crmoduleKey, 'imported')) {
+              cardPath = `<div style="text-align: center;"><img width="125" height="175" src="modules/alienrpg-corerules/images/cards/card-${broll.total}.png"></div>`;
             }
-          } catch (error) {}
-        }
+          } catch (error) {
+            try {
+              if (game.settings.get(ssmoduleKey, 'imported')) {
+                cardPath = `<div style="text-align: center;"><img width="125" height="175" src="modules/alienrpg-starterset/images/cards/card-${broll.total}.png"></div>`;
+              }
+            } catch (error) {}
+          }
 
-        // Construct chat message data
-        let messageData = mergeObject(
-          {
-            speaker: {
-              scene: canvas.scene.data._id,
-              actor: c.actor ? c.actor.data._id : null,
-              token: c.token.data._id,
-              alias: c.token.name,
+          // Construct chat message data
+          let messageData = mergeObject(
+            {
+              speaker: {
+                scene: canvas.scene.data._id,
+                actor: combatant.actor ? combatant.actor.data._id : null,
+                token: combatant.token.data._id,
+                alias: combatant.token.name,
+              },
+              flavor: `${combatant.token.name} rolls for Initiative! <br> ${cardPath}`,
+              flags: { 'core.initiativeRoll': true },
             },
-            flavor: `${c.token.name} rolls for Initiative! <br> ${cardPath}`,
-            flags: { 'core.initiativeRoll': true },
-          },
-          messageOptions
+            messageOptions
+          );
+
+          const chatData = await broll.toMessage(messageData, { create: false, rollMode });
+
+          // Play 1 sound for the whole rolled set
+          if (i > 0) chatData.sound = null;
+          messages.push(chatData);
+        }
+      } else {
+        ui.notifications.error(
+          `You can only automatically roll Initiative for 10 PCs/NPCs.  Roll initiative manually for the remaining actors or split in to multiple encounters in the Combat Tracker`
         );
-        const chatData = roll.toMessage(messageData, { create: false, rollMode });
-
-        // Play 1 sound for the whole rolled set
-        if (i > 0) chatData.sound = null;
-        messages.push(chatData);
-
-        // Return the Roll and the chat data
-        return results;
-      },
-      [[], []]
-    );
+      }
+    }
     if (!updates.length) return this;
 
     // Update multiple combatants
@@ -102,7 +100,6 @@ export default class AlienRPGCombat extends Combat {
     if (updateTurn) {
       await this.update({ turn: this.turns.findIndex((t) => t.data._id === currentId) });
     }
-
     // Create multiple chat messages
     await CONFIG.ChatMessage.documentClass.create(messages);
 
