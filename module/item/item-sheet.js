232ff67bf7f3ee9b91b1d196326f54150453b123
pwatson100 <paul@skynet2020.co.uk>
2022-08-14 14:22:38 +0100

    {{editor adjustments.

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -29,7 +29,7 @@ export class alienrpgItemSheet extends ItemSheet {
   /* -------------------------------------------- */
 
   /** @override */
-  getData() {
+  async getData() {
     const data = super.getData();
     // const item = foundry.utils.deepClone(this.item.data);
     // debugger;
