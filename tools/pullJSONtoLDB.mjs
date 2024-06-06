ed378d9ebb7d8e07608052493bc906219e407cc1
pwatson100 <paul@skynet2020.co.uk>
2024-06-06 14:00:31 +0100

    Path Adjustments

--- tools/pullJSONtoLDB.mjs
+++ tools/pullJSONtoLDB.mjs
@@ -4,9 +4,9 @@ import { promises as fs } from 'fs';
 const MODULE_ID = process.cwd();
 const yaml = false;
 
-const packs = await fs.readdir('./packs/source');
+const packs = await fs.readdir('./src/packs');
 for (const pack of packs) {
 	if (pack === '.gitattributes') continue;
 	console.log('Packing ' + pack);
-	await compilePack(`${MODULE_ID}/packs/source/${pack}`, `${MODULE_ID}/packs/${pack}`, { yaml });
+	await compilePack(`${MODULE_ID}/src/packs/${pack}`, `${MODULE_ID}/packs/${pack}`, { yaml });
 }
--- tools/pushLDBtoJSON.mjs
+++ tools/pushLDBtoJSON.mjs
@@ -9,7 +9,7 @@ const packs = await fs.readdir('./packs');
 for (const pack of packs) {
 	if (pack === '.gitattributes') continue;
 	console.log('Unpacking ' + pack);
-	const directory = `./packs/source/${pack}`;
+	const directory = `./src/packs/${pack}`;
 	try {
 		for (const file of await fs.readdir(directory)) {
 			await fs.unlink(path.join(directory, file));
@@ -18,7 +18,7 @@ for (const pack of packs) {
 		if (error.code === 'ENOENT') console.log('No files inside of ' + pack);
 		else console.log(error);
 	}
-	await extractPack(`${MODULE_ID}/packs/${pack}`, `${MODULE_ID}/packs/source/${pack}`, {
+	await extractPack(`${MODULE_ID}/packs/${pack}`, `${MODULE_ID}/src/packs/${pack}`, {
 		yaml,
 		transformName,
 	});
